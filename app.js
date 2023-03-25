b9b7615aedd4322d9d3a59566e9438bd77ba6ac8
varun <s@gmail.com>
2023-03-25 23:50:47 +0530

    Added Babel

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+
+const Title = () => {
+  return <h1>Title component</h1>;
+};
+
+const HeaderComp = () => {
+  return (
+    <div>
+      {Title()}
+      {/* <Title/> */}
+      {/* <Title></Title> */}
+      <h1>Header Component</h1>
+      <h2>h2 tag </h2>
+    </div>
+  );
+};
+const head = <h1>Hello React</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeaderComp />);
+
+//render() - converts the heading object to respective tag and put it in the DOM
+//Modify the DOM
+//Override not append
+
 /**
  *
  *  <div id="parent">
@@ -10,21 +36,17 @@ import ReactDOM from 'react-dom/client'
  *  </div>
  *
  */
-const paragraph = React.createElement("p", {}, "Paragraph");
 
-const heading = React.createElement("h1", {}, "Hello From React!! :fire");
+// const paragraph = React.createElement("p", {}, "Paragraph");
 
-const child = React.createElement("div", { id: "child" }, heading, paragraph);
+// const heading = React.createElement("h1", {}, "Hello From React!! :fire");
 
-const parent = React.createElement("div", { id: "parent" }, child);
+// const child = React.createElement("div", { id: "child" }, heading, paragraph);
 
-console.log(heading); //returns object
+// const parent = React.createElement("div", { id: "parent" }, child);
+
+// console.log(heading); returns object
+
+// React.createElement => returns object => HTMl => puts it upon DOM
 
 // we add the tags into the root using REACT DOM not the REACT
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-
-root.render(parent);
-
-//render() - converts the heading object to respective tag and put it in the DOM
--- read-me.md
+++ read-me.md
@@ -12,8 +12,8 @@
 - React is not the only thing which makes an app fast along with React bundlers like Vite, Parcel, Webpack helps 
 - Parcel uses Consistent Hashing
 - It also does Code Splitting
-- Differential Bundling - support older browsers
+- Differential Bundling - Support older browsers
 - Diagnostics, Better Error Handling
 - Parcel also helps in HTTPS 
-- Tree Shaking - remove unused code for us
-- Different Dev and Production Bundles
+- Tree Shaking - Remove unused code for us
+- Different Dev and Production Bundles.
